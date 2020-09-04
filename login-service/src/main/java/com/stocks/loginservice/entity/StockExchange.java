ecc3871c4b3c0e3bade9416609d6a0061bb5848c
rish-av <rishkumar2345@gmail.com>
2020-09-04 14:35:01 +0530

    New Repo

---
+++ login-service/src/main/java/com/stocks/loginservice/entity/StockExchange.java
@@ -0,0 +1,81 @@
+package com.stocks.loginservice.entity;
+
+import java.util.Set;
+
+import javax.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+@Entity
+@JsonIdentityInfo(
+        scope = StockExchange.class,
+        generator = ObjectIdGenerators.PropertyGenerator.class,
+        property = "id")
+public class StockExchange {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String name;
+    private String brief;
+    private String remarks;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "contact_id", nullable = false)
+    private Contact contact;
+
+    @ManyToMany(mappedBy = "stockExchanges", cascade = CascadeType.MERGE)
+    private Set<Company> companies;
+
+    public Contact getContact() {
+        return contact;
+    }
+
+    public void setContact(Contact contact) {
+        this.contact = contact;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrief() {
+        return brief;
+    }
+
+    public void setBrief(String brief) {
+        this.brief = brief;
+    }
+
+    public String getRemarks() {
+        return remarks;
+    }
+
+    public void setRemarks(String remarks) {
+        this.remarks = remarks;
+    }
+
+    @JsonIgnore
+    public Set<Company> getCompanies() {
+        return companies;
+    }
+
+    public void setCompanies(Set<Company> companies) {
+        this.companies = companies;
+    }
+}
